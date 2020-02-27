cd26a07b2fdc013440f2283e6a9140a85f0a47e3
Benjamin Wood <ben@hint.io>
2020-02-26 16:49:13 -0800

    Support sprockets 3 & 4
    And resolve deprecation warnings at sprockets 3

--- lib/hogan_assets/engine.rb
+++ lib/hogan_assets/engine.rb
@@ -4,7 +4,18 @@ module HoganAssets
       HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
       Rails.application.config.assets.configure do |env|
         HoganAssets::Config.template_extensions.each do |ext|
-          env.register_engine(".#{ext}", Tilt)
+
+          mime_type = 'application/javascript'
+          extension = ".#{ext}"
+
+          if env.respond_to?(:register_transformer)
+            env.register_mime_type mime_type, extensions: [extension]
+            env.register_preprocessor mime_type, Tilt
+          elsif env.respond_to?(:register_engine)
+            args = [extension, Tilt]
+            args << { mime_type: mime_type, silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+            env.register_engine(*args)
+          end
         end
       end
     end
