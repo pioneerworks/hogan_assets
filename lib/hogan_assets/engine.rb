9e21112d913a96490eb2bb73d2a5e5959eaedbed
Anton Orel <eagle.anton@gmail.com>
2021-03-05 13:04:00 -0500

    Suppresses a deprecation message
    ```
    DEPRECATION WARNING: Sprockets method `register_engine` is deprecated.
    Please register a mime type using `register_mime_type` then
    use `register_compressor` or `register_transformer`.
    https://github.com/rails/sprockets/blob/master/guides/extending_sprockets.md#supporting-all-versions-of-sprockets-in-processors
     (called from block (3 levels) in <class:Engine> at /home/anton/.rbenv/versions/2.6.5/lib/ruby/gems/2.6.0/bundler/gems/hogan_assets-c69aa916dad2/lib/hogan_assets/engine.rb:7)
    ```

--- lib/hogan_assets/engine.rb
+++ lib/hogan_assets/engine.rb
@@ -1,10 +1,10 @@
 module HoganAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.hogan", :group => :all do |app|
+    initializer 'sprockets.hogan', group: :all do |_app|
       HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
       Rails.application.config.assets.configure do |env|
         HoganAssets::Config.template_extensions.each do |ext|
-          env.register_engine(".#{ext}", Tilt)
+          env.register_engine(".#{ext}", Tilt, silence_deprecation: true)
         end
       end
     end
